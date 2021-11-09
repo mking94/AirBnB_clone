484b7c0c25abd7268f19d242e6963d937aa84b7c
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-11-09 08:01:04 -0500

    BaseModel Class

---
+++ models/base_model.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import models
+from uuid import uuid4
+from datetime import datetime
+
+class BaseModel:
+    """ Defin BaseModel class"""
+    def __init__(self):
+        """ Constructor """
+        self.id = str(uuid4())
+        self.created_at = datetime.today().strftime("%b %d %Y %H:%M:%S")
+        self.updated_at = datetime.today().strftime("%b %d %Y %H:%M:%S")
+     def __str__(self):
+        """Return string."""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+     def save(self):
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.today()
+        models.storage.save()
