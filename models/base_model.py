65890b4ae5250cc722acc4c1bf1defcaa2a56f56
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-11-09 21:56:34 +0100

    BaseModel Class

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import models
 from uuid import uuid4
 from datetime import datetime
 
@@ -8,13 +7,13 @@ class BaseModel:
     def __init__(self):
         """ Constructor """
         self.id = str(uuid4())
-        self.created_at = datetime.today().strftime("%Y-%d-%m %H:%M:%S.%f")
-        self.updated_at = datetime.today().strftime("%Y-%d-%m %H:%M:%S.%f")
+        self.created_at = datetime.today().strftime('%Y, %m, %d %H:%M:%S.%f')
+        self.updated_at = datetime.today().strftime('%Y, %m, %d %H:%M:%S.%f')
      def __str__(self):
         """Return string."""
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
      def save(self):
         """Update updated_at with the current datetime."""
-        self.updated_at = datetime.today()
+        self.updated_at = datetime.today().strftime('%Y, %m, %d %H:%M:%S.%f')
         models.storage.save()
